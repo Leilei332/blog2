49db195fbfc30b97a2d3990cdcde9ba708264e0b
Leilei332 <LeiYiXia29@outlook.com>
2025-11-08 11:36:03 +0800

    Add netlify support

--- src/config.ts
+++ src/config.ts
@@ -14,7 +14,7 @@ export const themeConfig: ThemeConfig = {
     // author name
     author: 'XLBilly',
     // site url
-    url: 'https://retypeset.radishzz.cc',
+    url: 'https://xlbil.netlify.app',
     // base path
     // root directory for all pages and assets
     base: '/', // e.g., '/blog', '/docs'
